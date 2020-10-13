67f156a07ced202b05baa29fd3549d6955886d4a
Abdul Mohaimen <mohaimen707@gmail.com>
2020-10-14 00:24:28 +0600

    Declare all routes

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const contactRouter = require('./route');
 const app = express();
+app.use(morgan('dev'))
+
+app.use('/contact', contactRouter);
+app.get('*', (req, res) => {
+    res.send('404 Not Found!');
+})
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
