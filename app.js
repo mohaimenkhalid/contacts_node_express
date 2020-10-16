541e84b39a2b07a8b0d3b6027f3014de3ef97954
Abdul Mohaimen <mohaimen707@gmail.com>
2020-10-16 12:53:18 +0600

    Ejs template Engine include

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const morgan = require('morgan')
 const contactRouter = require('./route');
 const app = express();
 
+app.set('view engine', 'ejs') //change view Engine
+
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+app.get('/about', (req, res) => {
+    res.render('pages/about')
+})
+app.get('/', (req, res) =>{
+    let post = {
+        title: 'Test title',
+        body: 'Test body',
+        published : true
+    }
+    res.render('pages/index', {title: 'EJS is a template Engine', post : post})
+});
 app.use('/contact', contactRouter);
 app.get('*', (req, res) => {
     res.send('404 Not Found!');
